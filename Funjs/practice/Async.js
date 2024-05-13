dee8dcd1b708cc9d03589e72078317aefcb27a45
piyush254 <76161817+piyush254@users.noreply.github.com>
2024-05-13 20:15:16 +0530

    lexical and Async

--- Funjs/practice/Async.js
+++ Funjs/practice/Async.js
@@ -23,14 +23,16 @@ function syaHello() {
 
 //setInterval
 
-
+ let time  = document.querySelector("#time1");
 const intervalMe =  setInterval(() => {
   const now = new Date();
+  time.innerHTML = `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`
   console.log(`${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`);
 }, 1000);
 
 
 document.querySelector("#stop").addEventListener('click', ()=>{
   console.log("Interval is is stop");
+  time.innerHTML = `You stop time`
   clearTimeout(intervalMe);
  })
